f1aaca617ea291e921d5a902eaf058f2977c4cce
greencardamom <seucxzya0c8dmle4tulz@nym.hush.com>
2026-01-27 20:34:47 -0500

    2026-01-27

--- glacier.py
+++ glacier.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
+
+#
+# Copyright (c) 2026 Greencardamom
+# 
+# This file is part of Glacier Mirror.
+# https://github.com/greencardamom/Glacier
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# See LICENSE.md for more details.
+#
+
 import os
 import sys
 
